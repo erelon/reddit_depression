f47c81b3902dc5d70dfac62165a078379c0d2dc6
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-10-23 22:10:11 +0300

    adding more plots

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -1,14 +1,14 @@
 import torch
 
 from models.lightning_model import Lightning_Super_Model
-from torchvision.models import vgg11_bn
+from torchvision.models import googlenet
 
 
 class CNN_Model(Lightning_Super_Model):
     def __init__(self):
         super().__init__()
         self.cnn = torch.nn.Conv2d(1, 3, (1, 1))
-        self.vgg = vgg11_bn(pretrained=True)
+        self.vgg = googlenet(pretrained=True)
         self.vgg.classifier = torch.nn.Sequential(
             torch.nn.Linear(512 * 7 * 7, 4096),
             torch.nn.ReLU(True),
